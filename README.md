da1cc16b626959bbfa640b7ec70c260d37870e9b
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2022-11-20 06:48:13 +0700

    Add nik.py

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 - `data-input`: region code data
 - `data.py`: data input & wrangling
 - `reader.py`: function(s) to process the data
+- `nik.py`: individual functions to get values from NIK (in development)
 - `temp.ipynb` & `temp.py`: playgrounds to test the code
---
+++ nik.py
@@ -0,0 +1,35 @@
+def province():
+    prov = ...
+    return prov
+
+def city():
+    city = ...
+    return city
+
+def district():
+    dist = ...
+    return dist
+
+def gender():
+    gend = ...
+    return gend
+
+def birthdate():
+    bdate = ...
+    return bdate
+
+def birthmonth():
+    bmonth = ...
+    return bmonth
+
+def birthyear():
+    byear = ...
+    return byear
+
+def birthdtm(): # birthday in datetime data type
+    bdtm = ...
+    return bdtm
+
+def birthday(): # birthday in string data type
+    bday = ...
+    return bday
