a33b192c37b96916ba4825a12d0a9a574c90a55f
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2022-11-30 10:16:15 +0700

    Add kk function in reader.py

--- reader.py
+++ reader.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
+from data import regioncode as rc
 
-def nik(idnum, data):
+def nik(idnum):
     idnum = str(idnum)
     prov_code = idnum[:2] # province
     city_code = f'{prov_code}.{idnum[2:4]}' # city
@@ -10,9 +11,9 @@ def nik(idnum, data):
     birth_year = idnum[10:12] # birthday year
     nth_person = int(idnum[13:]) # the nth person being recorded
 
-    prov = data.loc[data['code'] == prov_code, 'region'].item()
-    city = data.loc[data['code'] == city_code, 'region'].item()
-    dist = data.loc[data['code'] == dist_code, 'region'].item()
+    prov = rc.loc[rc['code'] == prov_code, 'region'].item()
+    city = rc.loc[rc['code'] == city_code, 'region'].item()
+    dist = rc.loc[rc['code'] == dist_code, 'region'].item()
 
     if birth_code >= 1 and birth_code <= 31:
         gender = "Male"
@@ -24,11 +25,30 @@ def nik(idnum, data):
         raise("birth_date must be between 1 to 71")
 
     birth_str = f'{birth_date}/{birth_month}/{birth_year}'
-    birth_raw = datetime.strptime(birth_str, '%d/%m/%y')
-    birthday = birth_raw.strftime("%d %B %Y")
+    birth_dttm = datetime.strptime(birth_str, '%d/%m/%y')
+    birthday = birth_dttm.strftime("%d %B %Y")
     
     today = date.today()
-    age = today.year - birth_raw.year - ((today.month, today.day) < (birth_raw.month, birth_raw.day))
+    age = today.year - birth_dttm.year - ((today.month, today.day) < (birth_dttm.month, birth_dttm.day))
 
     return prov, city, dist, gender, birthday, age, nth_person
     
+def kk(idnum):
+    idnum = str(idnum)
+    prov_code = idnum[:2] # province
+    city_code = f'{prov_code}.{idnum[2:4]}' # city
+    dist_code = f'{city_code}.{idnum[4:6]}' # district / kecamatan
+    reg_date = int(idnum[6:8]) # registration date
+    reg_month = idnum[8:10] # birthday month
+    reg_year = idnum[10:12] # birthday year
+    nth_pub = int(idnum[13:]) # the nth KK being published
+
+    prov = rc.loc[rc['code'] == prov_code, 'region'].item()
+    city = rc.loc[rc['code'] == city_code, 'region'].item()
+    dist = rc.loc[rc['code'] == dist_code, 'region'].item()
+
+    reg_str = f'{reg_date}/{reg_month}/{reg_year}'
+    reg_dttm = datetime.strptime(reg_str, '%d/%m/%y')
+    reg = reg_dttm.strftime("%d %B %Y")
+
+    return prov, city, dist, reg_dttm, reg, nth_pub
